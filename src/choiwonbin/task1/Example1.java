3d6641e239f88e0f2959faa2fb82abd22e900770
beatitudo <beatitudo331@gmail.com>
2023-01-09 23:34:20 +0900

    Initial commit

---
+++ src/choiwonbin/task1/Example1.java
@@ -0,0 +1,15 @@
+package choiwonbin.task1;
+
+public class Example1 {
+    private int num;
+
+    public static void main(String[] args) {
+        int num;
+
+        Example1 example1 = new Example1();
+        System.out.println("example1.num = " + example1.num);
+
+        num = 10;
+        System.out.println("num = " + num);
+    }
+}
---
+++ src/choiwonbin/task2/Example1.java
@@ -0,0 +1,18 @@
+package choiwonbin.task2;
+
+public class Example1 {
+    private static final String number = "8";
+    
+    public static void main(String[] args) {
+        
+        int number = 5;
+        
+        System.out.println("Example1.number = " + Example1.number);
+        System.out.println("number = " + number);
+
+        System.out.println("Example1 + num = " + Example1.number + number);
+
+        System.out.print("Integer.parseInt(Example1.number) + number = ");
+        System.out.println(Integer.parseInt(Example1.number) + number);
+    }
+}
