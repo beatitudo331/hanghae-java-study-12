7f39d04bc98ceb81a45ba431ef3303054310187d
aihtnyc_h <feiver@naver.com>
2023-01-18 18:05:52 +0900

    hyunah
    박현아:Report1,Report2

---
+++ src/parkhyunah/report1/Report1_3.java
@@ -0,0 +1,21 @@
+package parkhyunah.report1;
+// 2-8 번 문제   <- 이렇게 문제 번호 작성 필수
+public class Exercise2_8 {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        int z = 3;
+
+        System.out.println("----------------------");
+        x++;
+        ++y;
+        --z;
+        --z;
+
+
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+        System.out.println("z="+z);
+    }
+}
+
