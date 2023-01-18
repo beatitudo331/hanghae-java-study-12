33b3540acc7380921a3854926e8ac7c1e8b80cef
choincnp <whdudwns7777@gmail.com>
2023-01-18 14:07:21 +0900

    feat: report1,report2 재제출

---
+++ src/choyoungjun/report1/Report1_3.java
@@ -0,0 +1,18 @@
+package choyoungjun.report1;
+
+// 2-8번 문제
+public class Report1_3 {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        int z = 3;
+        x = 2;
+        y = 3;
+        z = 1;
+
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+        System.out.println("z="+z);
+    }
+}
+//예상 결과 : x=2, y=3, z=1
