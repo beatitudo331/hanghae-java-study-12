ea4b067beeddef842d2834c48119c8b2270d8ee5
Jiyoon <51226340+trueyoon@users.noreply.github.com>
2023-01-18 16:38:16 +0900

    test

---
+++ src/hanjiyoon/report1/Report1_3.java
@@ -0,0 +1,23 @@
+package hanjiyoon.report1;
+// 2-8 번 문제   <- 이렇게 문제 번호 작성 필수
+public class Report1_3 {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        int z = 3;
+
+        int temp = x;
+        x = y;
+        y = z;
+        z = temp;
+
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+        System.out.println("z="+z);
+    }
+}
+//예상 결과 : x=2, y=3, z=1
+
+    }
+}
+
