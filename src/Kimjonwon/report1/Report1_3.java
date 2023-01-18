eb307fe46b309f6a673e8bd86c398acc6789c1bf
skwwnl <57902567+skwwnl@users.noreply.github.com>
2023-01-18 19:14:51 +0900

    내 자료 업로드

---
+++ src/Kimjonwon/report1/Report1_3.java
@@ -0,0 +1,29 @@
+package Kimjonwon.report1;
+// 2-8 번 문제   <- 이렇게 문제 번호 작성 필수
+public class Report1_3 {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        int z = 3;
+        int tmp;
+
+        tmp = x;
+        x = y;
+        y = tmp;
+
+        tmp = y;
+        y = z;
+        z = tmp;
+
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+        System.out.println("z="+z);
+    }
+}
+
+// 필요하다면 클래스 추가
+class AddClass {
+    void test() {
+        System.out.println("AddClass.test");
+    }
+}
