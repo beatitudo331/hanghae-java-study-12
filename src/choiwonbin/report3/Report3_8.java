f567934d3108edd46ecf60d367ce73b8a2559919
github oyg01285@naver.com <oyg01285@naver.com>
2023-01-18 22:21:16 +0900

    Report3일차 작성중

---
+++ src/choiwonbin/report3/Report3_8.java
@@ -0,0 +1,16 @@
+package choiwonbin.report3;
+
+public class Report3_8 {
+    public static void main(String[] args) {
+
+        //4-8. Math.random()을 이용해서 1부터 6 사이의 임의의 정수를 변수 value에 저장하는 코드를 완성하세요.
+
+        int value = ((int) (Math.random() * 6) + 1);
+
+        System.out.println("value =" + value) ;
+
+    }
+
+
+}
+
