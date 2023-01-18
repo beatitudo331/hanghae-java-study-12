17d7c145aa5379f2b551fc206292eff721ca867e
kikidy12 <kikidy12@naver.com>
2023-01-18 16:56:59 +0900

    1일차 과제 제출

--- src/choiwonbin/report1/Report1_3.java
+++ src/choiwonbin/report1/Report1_3.java
@@ -1,17 +1,23 @@
 package choiwonbin.report1;
-// 2-8 번 문제   <- 이렇게 문제 번호 작성 필수
-public class Report1_3 {
-    public static void main(String[] args) {
-        // 정답 작성
-        // Ex)
-        AddClass addClass = new AddClass();
-        addClass.test();
-    }
-}
+//2-8. 아래는 변수 x, y, z의 값을 서로 바꾸는 예제이다. 결과와 같이 출력되도록 코드를 넣으세요.
+//예상 결과 : x=2, y=3, z=1
 
-// 필요하다면 클래스 추가
-class AddClass {
-    void test() {
-        System.out.println("AddClass.test");
+class Exercise2_8 {
+    public static void main(String[] args){
+
+        int x = 1;
+        int y = 2;
+        int z = 3;
+       /*
+          알맞은 코드를 넣어 완성하세요.
+       */
+        int temp = x;
+        x = y;
+        y = z;
+        z = temp;
+
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+        System.out.println("z="+z);
     }
 }
