f98464bd6b8b1cbc34b57831e20377b3e65d996c
kikidy12 <kikidy12@naver.com>
2023-01-18 19:24:48 +0900

    1일차 과제 제출 (패키지 수정)

--- src/kwonseongmin/report2/Report2_4.java
+++ src/kwonseongmin/report2/Report2_4.java
@@ -1,4 +1,4 @@
-package kwonseongmin.report1.report2;
+package kwonseongmin.report2;
 
 //3-4. 아래의 코드는 사과를 담는데 필요한 바구니(버켓)의 수를 구하는 코드이다.
 //만일 사과의 수가 123개이고 하나의 바구니에는 10개의 사과를 담을 수 있다면, 13개의 바구니가 필요할 것이다.
--- src/kwonseongmin/report2/Report2_6.java
+++ src/kwonseongmin/report2/Report2_6.java
@@ -1,4 +1,4 @@
-package kwonseongmin.report1.report2;
+package kwonseongmin.report2;
 
 //3-6. 아래는 화씨(Fahrenheit)를 섭씨(Celcius)로 변환하는 코드이다.
 //변환 공식이 'C = 5/9*(F-32)'라고 할 때, 빈 칸에 알맞은 코드를 넣으시오.
