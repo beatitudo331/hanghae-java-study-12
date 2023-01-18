0990dd3f6603273e7aa1e646226e11a63d9ce5fd
siyaness <siyaness@naver.com>
2023-01-18 16:54:24 +0900

    java study

---
+++ src/koyoungjin/report2/Report2_4.java
@@ -0,0 +1,17 @@
+package koyoungjin.report2;
+// 3-4번 문제
+public class Report2_4 {
+        public static void main(String[] args){
+
+            int numOfApples = 123; // 사과의 개수
+            int sizeOfBucket = 10; // 바구니의 크기(바구니에 담을 수 있는 사과의 개수)
+            int numOfBucket = numOfApples/sizeOfBucket + (numOfApples%sizeOfBucket> 0 ? 1 : 0); // 모든 사과를 담는데 필요한 바구니의 수
+            System.out.println("필요한 바구니의 수 :"+numOfBucket);
+        }
+        //예상 결과 -> 필요한 바구니의 수 :13
+        /*
+        사과의 개수/바구니의 크기 = 바구니 12개
+        사과의 개수%바구니의 크기 = 나머지 3 / 나머지가 3이라는 얘기는 바구니가 하나 더 필요하단 얘기니까
+        삼항연산자 ?를 이용해 나머지가 1이상 나오면 (바구니가 1개 더 필요하기 때문에)1을 반환. (나머지가 없을 경우는 필요가 없기 때문에 0)
+         */
+}
---
+++ src/koyoungjin/report2/koyoungjin/report1/Report1_3.java
@@ -0,0 +1,23 @@
+package koyoungjin.report2.koyoungjin.report1;
+// 2-8 번 문제
+public class Report1_3 {
+    public static void main(String[] args){
+        int x = 1;
+        int y = 2;
+        int z = 3;
+
+        //알맞은 코드를 넣어 완성하세요.
+        int tmp;
+
+        tmp = x;
+        x = y;
+        y = z;
+        z = tmp;
+
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+        System.out.println("z="+z);
+
+        //예상 결과 : x=2, y=3, z=1
+    }
+}
