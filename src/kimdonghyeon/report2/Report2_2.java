b3842dc2a529cca9088c388fe31aaea7e80a8399
dkssudtp1 <dkssudtp1@naver.com>
2023-01-18 20:05:22 +0900

    style : 주석 고침

--- src/kimdonghyeon/report2/Report2_2.java
+++ src/kimdonghyeon/report2/Report2_2.java
@@ -11,7 +11,7 @@ public class Report2_2 {
         System.out.println(x += 2); //위쪽 후위 연산자에 위해 3이되고 +2가 실행되어 5가나옴
         System.out.println(!('A' <= c && c <= 'Z')); // true && true는 true고 느낌표가 붙어 not이 되어 false가 됨
         System.out.println('C' - c); //'C'는 문자코드로 67이므로 67-65가 됨
-        System.out.println('5' - '0'); //85 - 80 문자코드도 80부터 0~9까지 순서대로 있기때문에 5가 나옴
+        System.out.println('5' - '0'); //53 - 48 문자코드도 48부터 0~9까지 순서대로 있기때문에 5가 나옴
         System.out.println(c + 1); //A의 문자코드는 65이기때문에 +1 하면 66
         System.out.println(++c);//전위 연산자는 코드가 실행되기전에 적용되어 문자코드 66인 B로 나오게됨
         System.out.println(c++);//후위 연산자이므로 코드가 실행된후 다음코드부터 적용
