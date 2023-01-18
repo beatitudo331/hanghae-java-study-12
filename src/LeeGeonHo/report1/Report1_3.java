23df8462212cc2d2568e88828f0dbfdc045513ec
Kuno1734 <dlsgus59@gmail.com>
2023-01-18 15:10:31 +0900

    이건호 과제 day1 완료

---
+++ src/LeeGeonHo/report1/Report1_3.java
@@ -0,0 +1,24 @@
+package LeeGeonHo.report1;
+
+
+//2-8. 아래는 변수 x, y, z의 값을 서로 바꾸는 예제이다. 결과와 같이 출력되도록 코드를 넣으세요.
+public class Report1_3{
+    public static void main(String[] args){
+        int x = 1;
+        int y = 2;
+        int z = 3;
+        int tmp = 0;
+
+        tmp =x;
+        x=y;
+        y=z;
+        z=tmp;
+
+
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+        System.out.println("z="+z);
+    }
+}
+//예상 결과 : x=2, y=3, z=1
+
