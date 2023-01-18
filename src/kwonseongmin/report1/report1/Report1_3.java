cd345864a0d67eb2d7357aaf798b64b6146e1f09
kikidy12 <kikidy12@naver.com>
2023-01-18 19:16:03 +0900

    1일차 과제 제출 (패키지 수정)

---
+++ src/kwonseongmin/report1/report1/Report1_3.java
@@ -0,0 +1,23 @@
+package kwonseongmin.report1.report1;
+//2-8. 아래는 변수 x, y, z의 값을 서로 바꾸는 예제이다. 결과와 같이 출력되도록 코드를 넣으세요.
+//예상 결과 : x=2, y=3, z=1
+
+class Exercise2_8 {
+    public static void main(String[] args){
+
+        int x = 1;
+        int y = 2;
+        int z = 3;
+       /*
+          알맞은 코드를 넣어 완성하세요.
+       */
+        int temp = x;
+        x = y;
+        y = z;
+        z = temp;
+
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+        System.out.println("z="+z);
+    }
+}
