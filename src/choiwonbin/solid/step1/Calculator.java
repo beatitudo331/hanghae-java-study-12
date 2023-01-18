99f0a72615044d924a12714975fbe32a66a207e6
Seyoung Oh <osy9536@naver.com>
2023-01-18 12:42:57 +0900

    Revert "PR Test"
    This reverts commit 78b639a4b2a4938c51ec332a50a2103c3e505b43.

--- src/choiwonbin/solid/step1/Calculator.java
+++ src/choiwonbin/solid/step1/Calculator.java
@@ -3,6 +3,7 @@ package choiwonbin.solid.step1;
 public class Calculator {
     public int calculate(String operator, int firstNumber, int secondNumber) {
         int answer = 0;
+
         if(operator.equals("+")){
             answer = firstNumber + secondNumber;
         }else if(operator.equals("-")){
