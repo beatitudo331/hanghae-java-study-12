9b7ecb664f0012a182aa545a2ea94a7b76e91bac
YeowonKIM <kkyoo152@gmail.com>
2023-01-18 21:58:19 +0900

    report1, 2

---
+++ src/kimyeowon/report1/Report1_3.java
@@ -0,0 +1,18 @@
+package kimyeowon.report1;
+// 2-8 번 문제
+public class Report1_3 {
+    public static void main(String[] args) {
+        int x =1, y=2, z=3;
+        int tmp;
+
+        tmp = x;
+        x = y;
+        y = z;
+        z = tmp;
+
+        System.out.println("x = "+x);
+        System.out.println("y = "+y);
+        System.out.println("z = "+z);
+    }
+}
+
