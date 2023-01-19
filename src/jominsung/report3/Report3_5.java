2e68eea005124f0c9bd5f4e3f8d3ef45528e1770
Ganpyeon <jms03096@naver.com>
2023-01-20 03:03:57 +0900

    JoMinSung Report3,4

---
+++ src/jominsung/report3/Report3_5.java
@@ -0,0 +1,19 @@
+package jominsung.report3;
+
+public class Report3_5 {
+    public static void main(String[] args) {
+        //4-5. 다음의 for문을 while문으로 변경하세요.
+        int i = 0;
+
+        while (i<=10){
+            int j = 0;
+            while (j<=i){
+                System.out.print("*");
+                j++;
+            }System.out.println();
+            i++;
+        }
+    }
+}//end of main
+// end of class
+
