e7ac794366cbc5961f25f5219d696aff99d18d6e
regular_kim <regular.jk.kim@gmail.com>
2023-01-19 19:29:13 +0900

    #3 fix  report complete

--- src/kimjeongkyu/report3/Report3_4.java
+++ src/kimjeongkyu/report3/Report3_4.java
@@ -5,17 +5,16 @@ package kimjeongkyu.report3;
 public class Report3_4 {
     public static void main(String[] args) {
         int sum = 0; // 총합을 저장할 변수
-//        int s = 1; // 값의 부호를 바꿔주는데 사용할 변수
+        int s = 1; // 값의 부호를 바꿔주는데 사용할 변수
         int num = 0;
-        while (sum <= 100) {
-            if (num % 2 == 0) {
-                sum += num;
-            } else {
-                sum -= num;
+        for (int i = 0; sum <100; i++) {
+            s *= -1;
+            sum += (num * s);
+            if (sum >= 100) {
+                break;
             }
             num++;
         }
-
         System.out.println("num=" + num);
         System.out.println("sum=" + sum);
     }
