9480c467aa3ebfbc5adcd1863022217c3e46098e
Kuno1734 <dlsgus59@gmail.com>
2023-01-19 16:27:53 +0900

    이건호 과제 day2 완료

---
+++ src/LeeGeonHo/report4/Report4_1.java
@@ -0,0 +1,12 @@
+package LeeGeonHo.report4;
+
+public class Report4_1 {
+    //다음은 배열을 선언하거나 초기화 한것이다. 잘못된 ㄱ럿을 고르고 그 이유를 설명하시오
+
+//    int[] arr[];
+//    int[] arr = {1,2,3};
+//    int[] arr = new int[5];
+//    int[] arr new int[5]{1,2,3,4,5}; // []에서 크기지정 할 수 없다 {}내의 데이터 개수로 크기지정
+//    int arr[5]; //배열의 선언이다. 배열을 선언할 때는 크기를 지정할 수 없다
+//    int[] arr[] = new int[];
+}
---
+++ src/LeeGeonHo/report4/Report4_2.java
@@ -0,0 +1,18 @@
+package LeeGeonHo.report4;
+
+public class Report4_2 {
+    public static void main(String[] args) {
+
+
+        //5-2. 다음과 같은 배열이 있을 때, arr[3].length의 값은?
+        int[][] arr = {
+                {5, 5, 5, 5, 5},
+                {10, 10, 10},
+                {20, 20, 20, 20},
+                {30, 30}
+
+        };
+    }
+}
+
+//정답 : 2
