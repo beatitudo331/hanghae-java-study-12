d5064769f3ef5e89c2032d8fd1b9afc3dc80bf6b
GEUNHOKIM <rmszkf@naver.com>
2023-01-20 02:49:58 +0900

    geunhokim Report 3,4 제출

---
+++ src/geunhokim/report3/report3_4.java
@@ -0,0 +1,33 @@
+package geunhokim.report3;
+
+//4-4. 1+(-2)+3+(-4)+...과 같은 식으로 계속 더해나갔을 때,
+//몇까지 더해야 총합이 100 이상이 되는지 구하세요.
+class Exercise4_4 {
+    public static void main(String[] args) {
+        int sum = 0; // 총합을 저장할 변수
+        int s = 1; // 값의 부호를 바꿔주는데 사용할 변수
+        int num = 0;
+        int i = 1;
+
+        while (sum <= 100) {
+            if (i % 2 == 0) {
+                s = -1;
+                num = s * i++;
+                sum += num;
+            } else {
+                s = 1;
+                num = s * i++;
+                sum += num;
+            }
+
+            if (sum >= 100) {
+                break;
+            }
+        }
+
+        System.out.println("num="+num);
+        System.out.println("sum="+sum);
+
+        }
+
+}
