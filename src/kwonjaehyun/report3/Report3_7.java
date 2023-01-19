bb5203fb53c6c1aa9dd08f87cf43a167ed46a84d
Galmaeki <kjh09800@gmail.com>
2023-01-19 17:16:59 +0900

    다이어그램 작성

--- src/kwonjaehyun/report3/Report3_7.java
+++ src/kwonjaehyun/report3/Report3_7.java
@@ -1,2 +1,13 @@
-package kwonjaehyun.report3;public class Report3_7 {
+package kwonjaehyun.report3;
+//4-7
+//4-7. 숫자로 이루어진 문자열 str이 있을 때, 각 자리의 합을 더한 결과를 출력하는 코드를 완성하세요.
+public class Report3_7 {
+    public static void main(String[] args) {
+        String str = "12345";
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            sum = (str.charAt(i)-48)+sum;
+        }
+        System.out.println("sum=" + sum);
+    }
 }
--- src/kwonjaehyun/report3/Report3_8.java
+++ src/kwonjaehyun/report3/Report3_8.java
@@ -1,2 +1,9 @@
-package kwonjaehyun.report3;public class Report3_8 {
+package kwonjaehyun.report3;
+//4-8번 문제
+//4-8. Math.random()을 이용해서 1부터 6 사이의 임의의 정수를 변수 value에 저장하는 코드를 완성하세요.
+public class Report3_8 {
+    public static void main(String[] args) {
+        int value = ((int)(Math.random()*6)+1);
+        System.out.println("value:"+value);
+    }
 }
