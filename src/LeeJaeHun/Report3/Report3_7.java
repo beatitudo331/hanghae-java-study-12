b974232f815bfadad36aad1f3ff4b1a656ad771d
JaeHun <clickyour@gmail.com>
2023-01-19 09:50:56 +0900

    2일차 과제

---
+++ src/LeeJaeHun/Report3/Report3_7.java
@@ -0,0 +1,18 @@
+//4-4. 1+(-2)+3+(-4)+...과 같은 식으로 계속 더해나갔을 때,
+//몇까지 더해야 총합이 100 이상이 되는지 구하세요.
+package LeeJaeHun.Report3;
+//4-7. 숫자로 이루어진 문자열 str이 있을 때, 각 자리의 합을 더한 결과를 출력하는 코드를 완성하세요.
+//만일 문자열이 "12345"라면, ‘1+2+3+4+5’의 결과인 15를 출력이 출력되어야 합니다.
+class Report3_7 {
+    public static void main(String[] args) {
+        String str = "12345";
+        int sum = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            sum += Integer.parseInt(String.valueOf(str.charAt(i)));
+             //sum += str.charAt(i)-48; 이것도가능
+        }
+        System.out.println(str.charAt(0));
+        System.out.println("sum=" + sum);
+    }
+}
---
+++ src/LeeJaeHun/Report3/Report3_8.java
@@ -0,0 +1,11 @@
+//4-4. 1+(-2)+3+(-4)+...과 같은 식으로 계속 더해나갔을 때,
+//몇까지 더해야 총합이 100 이상이 되는지 구하세요.
+package LeeJaeHun.Report3;
+
+//4-8. Math.random()을 이용해서 1부터 6 사이의 임의의 정수를 변수 value에 저장하는 코드를 완성하세요.
+class Report3_8 {
+    public static void main(String[] args){
+        int value = (int)(Math.random()*6)+1;
+        System.out.println("value:"+value);
+    }
+}
