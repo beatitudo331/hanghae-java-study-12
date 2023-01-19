b5643aeb4f3c3a99734ba8ea6735eb6762e8f60d
jan <samjan1127@gmail.com>
2023-01-19 21:54:46 +0900

    add: report3,4

---
+++ src/jeondabeen/report3/Report3_9.java
@@ -0,0 +1,17 @@
+package jeondabeen.report3;
+
+import java.util.Arrays;
+
+public class Report3_9 {
+    public static void main(String[] args) {
+        int num = 12345;
+        int sum = 0;
+
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        System.out.println("sum="+sum);
+    }
+}
