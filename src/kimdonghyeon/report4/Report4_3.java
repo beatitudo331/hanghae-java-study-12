fea9fb306224a515b8da832fb4b7aff8b02f1e1c
dkssudtp1 <dkssudtp1@naver.com>
2023-01-19 12:42:12 +0900

    김동현(kimdonghyeon) Report3, Report4 과제 제출

---
+++ src/kimdonghyeon/report4/Report4_3.java
@@ -0,0 +1,12 @@
+package kimdonghyeon.report4;
+
+//5-3. 배열 arr에 담긴 모든 값을 더하는 프로그램을 완성하세요.
+class Report4_3 {
+    public static void main(String[] args){
+        int[] arr = {10, 20, 30, 40, 50};
+        int sum = 0;
+        for(int i:arr)
+            sum += i;
+        System.out.println("sum="+sum);
+    }
+}//예상 결과 : sum=150
