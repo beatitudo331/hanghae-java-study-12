e5c4e4f7047e8df862703a2b5192d3b03fccfad9
jan <samjan1127@gmail.com>
2023-01-19 22:21:14 +0900

    modify: report1_3

--- src/jeondabeen/report1/Report1_3.java
+++ src/jeondabeen/report1/Report1_3.java
@@ -8,29 +8,12 @@ public class Report1_3 {
 
         int tmp = x;
         x = y;
-        y = tmp;
-        tmp = z;
-        z = y;
-        y = tmp;
+        y = z;
+        z = tmp;
 
         //예상 결과 : x=2, y=3, z=1
         System.out.println("x="+x);
         System.out.println("y="+y);
         System.out.println("z="+z);
-
-        byte b = 10;
-        char ch = 'A';
-        int i = 100;
-        long l = 1000L;
-
-        AddClass addClass = new AddClass();
-        addClass.test();
-    }
-}
-
-// 필요하다면 클래스 추가
-class AddClass {
-    void test() {
-        System.out.println("AddClass.test");
     }
 }
