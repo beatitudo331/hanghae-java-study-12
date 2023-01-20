cd907a0ae1c6b6bbe77b02d7145b38844e786de4
Seyoung Oh <osy9536@naver.com>
2023-01-20 12:31:51 +0900

    [modify] report 2-4, report 2-6

--- src/ohseyoung/report2/report2_6.java
+++ src/ohseyoung/report2/report2_6.java
@@ -3,7 +3,7 @@ package ohseyoung.report2;
 public class report2_6 {
     public static void main(String[] args) {
         int fahrenheit = 100;
-        float celcius = Float.parseFloat(String.format("%.2f",(float) 5/9*(fahrenheit-32)));
+        float celcius = (int)(5/9f*(fahrenheit-32)*100+0.5)/100f;
 
         System.out.println("Fahrenheit:"+fahrenheit);
         System.out.println("Celcius:"+celcius);
