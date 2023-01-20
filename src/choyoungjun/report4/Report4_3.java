d3d373e6ad5be76c327d034c386d7579b36c8aa7
choincnp <whdudwns7777@gmail.com>
2023-01-20 18:33:55 +0900

    feat: report3,report4 제출

---
+++ src/choyoungjun/report4/Report4_3.java
@@ -0,0 +1,10 @@
+package choyoungjun.report4;
+
+public class Report4_3 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int sum = 0;
+        for (int i : arr) sum += i;
+        System.out.println("sum="+sum);
+    }
+}
