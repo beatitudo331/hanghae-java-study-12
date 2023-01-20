117532d4bd50f214dbfa8abbf3e437f31c712fe4
kingaser <kignaser@naver.com>
2023-01-20 18:24:12 +0900

    강혜광(Kang HyeGwang) 2일차 3_2번 문제 수정

--- src/kanghyegwang/report3/Report3_2.java
+++ src/kanghyegwang/report3/Report3_2.java
@@ -14,7 +14,7 @@ class Exercise4_2 {
     public static void main(String[] args) {
         int sum = 0;
         for (int i = 1; i <= 20; i++) {
-            if (i%2 != 0 || i%3 != 0) {
+            if (!(i%2 == 0 || i%3 == 0)) {
                 sum += i;
             }
         }
