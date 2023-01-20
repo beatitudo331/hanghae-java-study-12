2e50bbf0da6185a12941fa7862896496043124e3
choincnp <whdudwns7777@gmail.com>
2023-01-20 18:13:31 +0900

    Revert "feat: report3 제출"
    This reverts commit 7a1c2abd3c3143976b3ae0fb7d65edebf03fdace.

--- src/choyoungjun/report3/Report3_6.java
+++
@@ -1,9 +0,0 @@
-package choyoungjun.report3;
-
-public class Report3_6 {
-    public static void main(String[] args) {
-        for (int i=1; i<6; i++){
-            System.out.println(i+" "+(6-i));
-        }
-    }
-}
