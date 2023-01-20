5a3e11b89f3cc6e7913d4bc5716dbef6966bf3fd
the1224 <asdz2003@gmail.com>
2023-01-20 13:46:03 +0900

    최정환(choijeonghwan) report3, report4 과제 제출

--- src/choijeonghwan/report3/Report3_5.java
+++ src/choijeonghwan/report3/Report3_5.java
@@ -2,13 +2,13 @@
 class Exercise4_5 {
     public static void main(String[] args) {
         int i=0;
-        int j=0;
         while (i<=10){
-            j=0;
+            int j=0;
             while (j<=i){
                 System.out.print("*");
                 j++;
             }
+            System.out.println();
             i++;
         }
 
