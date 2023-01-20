c7ed3e2a2aeb48693d338b3aaa265533be66356c
snagwon <ezlsw@naver.com>
2023-01-20 22:08:39 +0900

    과제 제출

---
+++ src/leesangwon/report3/Report3_6.java
@@ -0,0 +1,13 @@
+package leesangwon.report3;
+//4-6번 문제
+public class Report3_6 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 1; j <= 6; j++) {
+                if (i + j == 6) {
+                    System.out.println("(" + i + "," + j + ")");
+                }
+            }
+        }
+    }
+}
---
+++ src/leesangwon/report3/Report3_7.java
@@ -0,0 +1,14 @@
+package leesangwon.report3;
+//4-7번 문제
+public class Report3_7 {
+    public static void main(String[] args) {
+        String str = "12345";
+        int sum = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            sum += str.charAt(i) - '0';
+        }
+
+        System.out.println("sum=" + sum);
+    }
+}//예상 결과 : sum=15
