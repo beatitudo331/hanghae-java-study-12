7a1c2abd3c3143976b3ae0fb7d65edebf03fdace
choincnp <whdudwns7777@gmail.com>
2023-01-20 17:31:41 +0900

    feat: report3 제출

---
+++ src/choyoungjun/report3/Report3_6.java
@@ -0,0 +1,9 @@
+package choyoungjun.report3;
+
+public class Report3_6 {
+    public static void main(String[] args) {
+        for (int i=1; i<6; i++){
+            System.out.println(i+" "+(6-i));
+        }
+    }
+}
