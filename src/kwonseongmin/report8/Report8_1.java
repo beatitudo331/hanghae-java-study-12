7e3745ff5158643f9500b0009fd60ec7b55d7920
kikidy12 <kikidy12@naver.com>
2023-01-25 13:37:06 +0900

    Report 7

---
+++ src/kwonseongmin/report8/Report8_1.java
@@ -0,0 +1,13 @@
+package kwonseongmin.report8;
+
+
+
+class Exercise7_1 {
+    public static void main(String args[]) {
+        SutdaDeck deck = new SutdaDeck();
+
+        for (int i = 0; i < deck.cards.length; i++)
+            System.out.print(deck.cards[i] + ",");
+    }
+}
+// 예상결과) 1K,2,3K,4,5,6,7,8K,9,10,1,2,3,4,5,6,7,8,9,10,
