2e269ccd7e686d17e45f99798a5b2209d3a9509b
dkssudtp1 <dkssudtp1@naver.com>
2023-01-26 19:22:11 +0900

    김동현(kimdonghyeon) SOLID 과제 제출

--- src/kimdonghyeon/solid/step4/DisplayResult.java
+++ src/kimdonghyeon/solid/step4/DisplayResult.java
@@ -1,9 +1,7 @@
 package kimdonghyeon.solid.step4;
 
 public interface DisplayResult {
-<<<<<<< HEAD
+
     void displayResult(AbstractOperation operation, int firstNumber, int secondNumber) throws Exception;
-=======
-        public abstract void displayResult(AbstractOperation operation, int firstNumber, int secondNumber) throws Exception;
->>>>>>> origin/main
+
 }
--- src/kimdonghyeon/solid/step4/DisplayResultWithOperator.java
+++ src/kimdonghyeon/solid/step4/DisplayResultWithOperator.java
@@ -1,9 +1,7 @@
 package kimdonghyeon.solid.step4;
 
 public interface DisplayResultWithOperator {
-<<<<<<< HEAD
+
     void displayResultWithOperator(AbstractOperation operation, int firstNumber, int secondNumber) throws Exception;
-=======
-    public abstract void displayResultWithOperator(AbstractOperation operation, int firstNumber, int secondNumber) throws Exception;
->>>>>>> origin/main
 }
+
